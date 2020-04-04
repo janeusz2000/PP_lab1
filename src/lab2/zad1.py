2bb789820be69b44a25f95beb74ac0f7b5aec8a1
b3atriix <beata.kotra97@gmail.com>
2020-04-04 14:24:37 +0200

    lab 2

--- src/lab2/zad1.py
+++ src/lab2/zad1.py
@@ -4,8 +4,8 @@ import time
 import sounddevice as sd
 
 f_dist = [5, 10, 15, 20, 25, -5, -10, -15, -20, -25]
-ref_wav = fun.gen_simple(1, 0.5, 1046.5, "sin", -3)
-random_wav = [fun.gen_simple(1, 0.5, 1046 + x , "sin", -3) for x in f_dist]
+ref_wav = fun.gen_simple(1, 0.5, 1000, "sin", -3)
+random_wav = [fun.gen_simple(1, 0.5, 1000 + x, "sin", -3) for x in f_dist]
 
 if __name__ == '__main__':
     tests = f_dist * 5
@@ -13,12 +13,11 @@ if __name__ == '__main__':
     choices = []
     for test in tests:
         sd.play(ref_wav, fun.fs)
-        time.sleep(0.5)
+        time.sleep(1.5)
         sd.play(random_wav[f_dist.index(test)], fun.fs)
         choice = input('Który był wyższy [1/2]')
         choices.append(choice)
 
-    with open("../output/lab2/zad1_results.txt", "w") as f:
+    with open("../../output/lab2/zad1_results.txt", "w") as f:
         for (test, choice) in zip(tests, choices):
             f.write(f'Różnica {test}, wcisnieto {choice}\n')
-        f.close()
--- src/lab2/zad2.py
+++ src/lab2/zad2.py
@@ -3,33 +3,49 @@ import time
 import sounddevice as sd
 
 
-base_freq = 1046.5
+base_freq = 1000
 ref_wav = fun.gen_simple(1, 0.5, base_freq, "sin", -3)
-delta_f = 100
+delta_f = 15
 choices = []
 test_freq = []
-for it in range(0, 10):
+correct_ans = []
+prev_ans_correct = False
+
+for _ in range(0, 5):
+    new_wav = fun.gen_simple(1, 0.5, base_freq + delta_f, "sin", -3)
+
     sd.play(ref_wav, fun.fs)
-    time.sleep(0.5)
-    sd.play(fun.gen_simple(1, 0.5, base_freq + delta_f, "sin", -3))
+    time.sleep(1.5)
+    sd.play(new_wav, fun.fs)
+
     choice = input('Który ton byl wyzszy? [1/2]')
     choices.append(choice)
-    test_freq.append(str(base_freq - delta_f))
+    test_freq.append(delta_f)
 
-    # Staircase method
-    if choice == '1' and (base_freq > (base_freq + delta_f)):
-        delta_f += 30
-    elif choice == '2' and (base_freq < (base_freq + delta_f)):
-        delta_f += - 30
-    elif choice == '1' and (base_freq < (base_freq + delta_f)):
-        delta_f += - 30
-    elif choice == '2' and (base_freq > (base_freq + delta_f)):
-        delta_f += 30
+    # print(f'Base freq: {base_freq}, delta: {delta_f}, choises: {choices}, test_freq: {test_freq}')
 
-for a in range(0, len(test_freq)):
-    print(test_freq[a] + ' ' + choices[a])
+    if base_freq > (base_freq + delta_f):
+        correct = '1'
+    else:
+        correct = '2'
 
-# with open("../output/lab2/zad2_results.txt", "w") as f:
-#      for (test, choice) in zip(test_freq, choices):
-#          f.write(f'Różnica {test}, wcisnieto {choice}\n')
-#      f.close()
+    user_correct = choice == correct
+
+    if user_correct and prev_ans_correct:
+        if correct == '1':
+            delta_f += 4
+        else:
+            delta_f -= 4
+
+    if not user_correct:
+        if correct == '1':
+            delta_f -= 4
+        else:
+            delta_f += 4
+
+    correct_ans.append(user_correct)
+    prev_ans_correct = user_correct
+
+with open("../../output/lab2/zad2_results.txt", "w") as f:
+    for (test, ans) in zip(test_freq, correct_ans):
+        f.write(f'Różnica {test}, odpowiedz {ans}\n')
