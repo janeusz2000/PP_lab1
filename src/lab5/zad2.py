0cb3e205383938a59536c3603551b4653e1835e1
b3atriix <beata.kotra97@gmail.com>
2020-05-02 12:39:43 +0200

    zad 2 poprawione

--- src/lab5/zad2.py
+++ src/lab5/zad2.py
@@ -4,37 +4,28 @@ import numpy as np
 import sounddevice as sd
 from time import sleep
 
-# Global Variables
-fs = 48000
-max_int16 = 32767
 
 if __name__ == '__main__':
-    sine_wav = fun.gen_simple(2, 0.5, 1000, "sin", -3, dtype=np.int32)
+    sine_wav = fun.gen_simple(2, 0.5, 1000, "sin", -20, dtype=np.int32)
     sine_right = np.zeros(len(sine_wav), dtype=np.int16)
     stereo_sine = np.array([sine_wav.astype(np.int16), sine_right]).T
 
-    h1 = fun.gen_simple(2, 0.5, 2000, "sin", -3, dtype=np.int32)
-    h2 = fun.gen_simple(2, 0.5, 3000, "sin", -3, dtype=np.int32)
-    h3 = fun.gen_simple(2, 0.5, 4000, "sin", -3, dtype=np.int32)
+    h1 = fun.gen_simple(2, 0.5, 2000, "sin", -20, dtype=np.int32)
+    h2 = fun.gen_simple(2, 0.5, 3000, "sin", -20, dtype=np.int32)
+    h3 = fun.gen_simple(2, 0.5, 4000, "sin", -20, dtype=np.int32)
 
     hct = ((sine_wav + h1 + h2 + h3) / 4).astype(np.int16)
     stereo_hct = np.array([hct, sine_right]).T
-    sd.play(stereo_sine, fun.fs)
-    sleep(2.5)
-    sd.play(hct, fun.fs)
-    print(hct)
-    while user_input == 'y':
-        left = np.copy(ref)
-        left[int(fs / 4):int(3 * fs / 4)] += fun.gen_simple(0.5, 0.2, 1000, "sin", offset)
-        right = np.zeros(len(left), dtype=np.int16)
-
-        output = np.array((left, right)).T
-        sd.play(output, fs)
-        sleep(1.2)
-        user_input = input("Did you hear the tone? y/n ")
-
-        offset -= 1
-
-    input()
+    tone_amp = -20
+    user_input = ''
+    while user_input != 'y':
+        sine = fun.gen_simple(2, 0.5, 1000, "sin", tone_amp, dtype=np.int32)
+        stereo_sine = np.array([sine_wav.astype(np.int16), sine_right]).T
+        sd.play(stereo_sine, fun.fs)
+        sleep(2.5)
+        sd.play(stereo_hct, fun.fs)
+        user_input = input("Were the sound equal in loudness? y/n ")
+        tone_amp += 1
+    print(tone_amp - 1)
 
 
